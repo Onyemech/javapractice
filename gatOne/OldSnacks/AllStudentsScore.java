f22304734e5a4a40948585f8ab3ac84aa6af1985
Onyemechi Caleb <Onyemechicaleb4@gmail.com>
2024-10-24 15:50:00 +0100

    Got average

--- gatOne/OldSnacks/AllStudentsScore.java
+++ gatOne/OldSnacks/AllStudentsScore.java
@@ -27,26 +27,23 @@ public class AllStudentsScore {
 			Scanner input = new Scanner(System.in);
 
 			int sum = 0;
-			int [] totalSubResult = new int[numberOfSubjects];
+			
 
 			int [] [] scoreRecorder = new int[numberOfStudents] [numberOfSubjects];
 			for(int count = 0; count < numberOfStudents; count++) {
 				int numbering1 = count + 1;
 				for(int counter = 0; counter < numberOfSubjects ; counter++) {
 					int numbering2 = counter + 1;
-					System.out.print("Entering score for student "+numbering1+"\nEnter score for subject "+numbering2+" : ");
+					System.out.print("Entering score for student "+numbering1+"\nEnter score for subject "+numbering2+" : \n");
 						if(scoreRecorder[count] [counter] < 0 || scoreRecorder[count] [counter]  > 100) {
 							System.out.println("Invalid score");
 						}
 						else {
 							scoreRecorder[count] [counter] = input.nextInt();
 						}
-					for(int getTotal = 0; getTotal < totalSubResult.length; getTotal++) {
-						sum = scoreRecorder[count] [counter];
-						totalSubResult = totalSubResult + sum;
-					}
+					
 				}
-			
+					
 			}
 			return scoreRecorder;
 		}
@@ -59,16 +56,24 @@ public class AllStudentsScore {
 						System.out.println("\t=================================");
 					}
 				}
+				int [] [] newScoreRecorder = scoreRecorder;
 
 				for(int output = 0; output < scoreRecorder.length; output++) {
-
+					float total = 0;
 					int display = output + 1;
 					System.out.print("\tStudents "+display+" : ");
-					for(int output3 = 0; output3 < numberOfSubjects; output3++) {
-						System.out.print(" "+scoreRecorder[output] [output3]);
+
+					float averageCalculator = 0;
+					float average = 0;
+					for(int output2 = 0; output2 < numberOfSubjects; output2++) {
+						total += scoreRecorder[output] [output2];
+						averageCalculator++;
+						average = total /averageCalculator ;
+						System.out.print(" "+scoreRecorder[output] [output2]);
 					}
+					System.out.printf(" %d ",total," %2.f ",average);
 					System.out.println();
-				}
+				} 
 			System.out.println("\t=================================");
 			
 		}
