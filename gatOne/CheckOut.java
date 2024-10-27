3f049bce1a3f07bac3858a9b6737792e59dff76e
Onyemechi Caleb <Onyemechicaleb4@gmail.com>
2024-10-27 16:27:14 +0100

    Done

--- gatOne/CheckOut.java
+++ gatOne/CheckOut.java
@@ -23,7 +23,8 @@ public class CheckOut {
 		ArrayList<String> nameOfItems = new ArrayList<>();
 		ArrayList<Integer> numberOfUnit = new ArrayList<>();
 		ArrayList<Double> pricePerUnit = new ArrayList<>();
-
+		ArrayList<Double> totalPrice = new ArrayList<>();
+		double subTotal = 0;
 
 		do {
 			System.out.println("What did the user buy?");
@@ -38,9 +39,13 @@ public class CheckOut {
 			
 			System.out.println("How much per unit?");
 			double itemPrice = input.nextDouble();
-			
-			total += itemPrice;
+			pricePerUnit.add(itemPrice);
 
+			total += itemPrice * numberOfItems;
+
+			double totalCost = itemPrice * numberOfItems;
+			totalPrice.add(totalCost);
+			
 			input.nextLine();
 
 			System.out.println("Add more Items?");
@@ -51,11 +56,16 @@ public class CheckOut {
 				String cashierName = input.nextLine();
 
 				System.out.println("How much discount will he get?");
-				int discount = input.nextInt();
-
-				System.out.println("Number of pieces : "+pieces);
-				System.out.println("Number of unit : "+total);
-
+				int discountPrice = input.nextInt();
+			
+				double discount = total * discountPrice / 100;
+				double vat = total * 17.5 / 100;
+				
+				for (double price : totalPrice) {
+    					subTotal += price;
+				}
+			
+				double billTotal = subTotal - discount + vat;
 				
 			String displayResult = "SEMICOLON STORES\n" +
                        		"\tMAIN BRANCH\n" +
@@ -66,11 +76,64 @@ public class CheckOut {
                        		"\tCustomer Name: " + customerName + "\n" +
                        		"\t=============================================";
 			System.out.println("\t" + displayResult);
+			System.out.println("\tITEM	QTY	PRICE	TOTAL(NGN)");
+			System.out.println("\t---------------------------------------------");
 
 		for (int result = 0; result < nameOfItems.size(); result++) {
-    			System.out.println("\t" + nameOfItems.get(result) + " " + numberOfUnit.get(result));
+    			System.out.println("\t" + nameOfItems.get(result) + "	" + numberOfUnit.get(result)+ "	"+pricePerUnit.get(result)+"	"+totalPrice.get(result));
 		}
-				
+			System.out.println("\t---------------------------------------------");
+		String output2 = "\tSub Total: "+subTotal+"\n"+
+			"\tDiscount: "+discount+"\n"+
+			"\tVAT @ 17.50%: "+vat+	"\n"+
+			"\t=============================================";
+		System.out.println(output2);
+		System.out.println("\tBill Total:	"+billTotal);
+
+		System.out.println("\t=============================================");
+		System.out.println("\tTHIS IS NOT AN RECEIPT KINDLY PAY "+billTotal);
+		System.out.println("\t=============================================");
+		
+		System.out.println("How much did the customer give to you?");
+		double customerAmount = input.nextDouble();
+		
+		if(customerAmount > billTotal) {
+			double balance = customerAmount - billTotal;
+
+			String displayResult2 = "SEMICOLON STORES\n" +
+                       		"\tMAIN BRANCH\n" +
+                       		"\tLOCATION: 312, HERBERT MACAULAY WAY, SABO YABA, LAGOS.\n" +
+                       		"\tTEL: 03293828343\n" +
+                      		"\tDate: " + formattedDateTime + "\n" +
+                       		"\tCashier: " + cashierName + "\n" +
+                       		"\tCustomer Name: " + customerName + "\n" +
+                       		"\t=============================================";
+			System.out.println("\t" + displayResult2);
+			System.out.println("\tITEM	QTY	PRICE	TOTAL(NGN)");
+			System.out.println("\t---------------------------------------------");
+
+		for (int result = 0; result < nameOfItems.size(); result++) {
+    			System.out.println("\t" + nameOfItems.get(result) + "	" + numberOfUnit.get(result)+ "	"+pricePerUnit.get(result)+"	"+totalPrice.get(result));
+		}
+			System.out.println("\t---------------------------------------------");
+		String display = "\tSub Total: "+subTotal+"\n"+
+			"\tDiscount: "+discount+"\n"+
+			"\tVAT @ 17.50%: "+vat+	"\n"+
+			"\t=============================================";
+		System.out.println(display);
+		System.out.println("\tBill Total:	"+billTotal);
+		System.out.println("\tAmount Paid:	"+customerAmount);
+		System.out.println("\tBalance:	"+balance);
+
+		System.out.println("\t=============================================");
+		System.out.println("\tTHANK YOU FOR YOUR PATRONAGE ");
+		System.out.println("\t=============================================");
+		
+		
+			
+		}
+
+		else if(customerAmount < billTotal) {System.out.print("Invalid payment amount!!!");}
 	}
 
 }
