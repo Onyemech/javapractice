7c9056415c64f50593f6479e1a95092b3e9c53d2
Onyemechi Caleb <Onyemechicaleb4@gmail.com>
2024-10-27 10:43:08 +0100

    Trying to get the print format

--- gatOne/CheckOut.java
+++ gatOne/CheckOut.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CheckOut {
@@ -9,20 +10,51 @@ public class CheckOut {
 		System.out.println("What is the customer's Name ");
 		String customerName = input.nextLine();
 
+		String cashierChoice;
+		int pieces = 0;
+		int total = 0;
+		
+		ArrayList<String> nameOfItems = new ArrayList<>();
+		ArrayList<Integer> numberOfUnit = new ArrayList<>();
+		ArrayList<Double> pricePerUnit = new ArrayList<>();
+
+
 		do {
 			System.out.println("What did the user buy?");
-			String itemName = input.nextLine
-
+			String itemName = input.nextLine();
+			nameOfItems.add(itemName);
+				
 			System.out.println("How many pieces");
 			int numberOfItems = input.nextInt();
+			numberOfUnit.add(numberOfItems);
+
+			pieces += numberOfItems;
 			
 			System.out.println("How much per unit?");
 			double itemPrice = input.nextDouble();
-	
+			
+			total += itemPrice;
+
+			input.nextLine();
+
 			System.out.println("Add more Items?");
-			String cashierChoice = input.nextLine();
+			cashierChoice = input.nextLine();
 		}
-		
+			while(cashierChoice.toUpperCase().equals("YES"));
+				System.out.println("What is your name?");
+				String cashierName = input.nextLine();
+
+				System.out.println("How much discount will he get?");
+				int discount = input.nextInt();
+
+				System.out.println("Number of pieces : "+pieces);
+				System.out.println("Number of unit : "+unit);
+
+				for(String output : nameOfItems) 
+					System.out.print(output+"\n");
+				for(int result : nameOfItems) 
+					System.out.println(result);
+
 	}
 
 }
